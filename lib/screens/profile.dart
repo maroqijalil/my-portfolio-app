813738dcd6d95d4afd6fa25effbe48a72562a239
maroqijalil <jalilpro31@gmail.com>
2022-07-22 13:43:43 +0700

    feat: add favorited and profile views counter

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -9,6 +9,8 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  bool favoriteState = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,67 +23,109 @@ class _ProfileScreenState extends State<ProfileScreen> {
         ),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundImage: AssetImage("assets/imgs/jalil.png"),
-                    radius: 65,
+          child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage("assets/imgs/jalil.png"),
+                  radius: 65,
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  "Maroqi Jalil",
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w900,
                   ),
-                  SizedBox(height: 12,),
-                  Text(
-                    "Maroqi Jalil",
-                    style: TextStyle(
-                      fontSize: 26,
-                      fontWeight: FontWeight.w900,
+                ),
+                Text(
+                  "Software Engineer",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w100,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.location_pin,
+                      color: Colors.black54,
+                      size: 16,
                     ),
-                  ),
-                  Text(
-                    "Software Engineer",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w100,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.location_pin,
-                        color: Colors.black54,
-                        size: 16,
+                    Text(
+                      "Surabaya",
+                      style: TextStyle(
+                        fontSize: 14,
                       ),
-                      Text(
-                        "Surabaya",
-                        style: TextStyle(
-                          fontSize: 14,
-                        ),
-                      )
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Text("200"),
-                          Text("Favorited"),
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              )
-            ],
+                    )
+                  ],
+                ),
+                SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    const FavoriteInfoComponent(
+                      count: 200,
+                      title: "Favorited",
+                    ),
+                    Container(
+                      child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            favoriteState = !favoriteState;
+                          });
+                        },
+                        icon: favoriteState
+                            ? Icon(Icons.favorite_outlined)
+                            : Icon(Icons.favorite_border),
+                      ),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: Colors.black12),
+                    ),
+                    const FavoriteInfoComponent(
+                      count: 126,
+                      title: "Profile Views",
+                    )
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+class FavoriteInfoComponent extends StatelessWidget {
+  const FavoriteInfoComponent(
+      {Key? key, required this.count, required this.title})
+      : super(key: key);
+
+  final num count;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            count.toString(),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
           ),
-        ) 
+          Text(title),
+        ],
       ),
     );
   }
